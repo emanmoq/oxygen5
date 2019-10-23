3a9430c43d8c63e10034baad0e17ba54339b5987
emanmoq <42995029+emanmoq@users.noreply.github.com>
2019-10-23 12:54:27 -0700

    ff

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,21 @@ jQuery(function ($) {
 		new WOW().init();
 	}
 
-		
+	$('#features').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+		if (visible) {
+			$(this).find('.counter').each(function () {
+				var $this = $(this);
+				$({ Counter: 0 }).animate({ Counter: $this.text() }, {
+					duration: 2000,
+					easing: 'swing',
+					step: function () {
+						$this.text(Math.ceil(this.Counter));
+					}
+				});
+			});
+			$(this).unbind('inview');
+		}
+	});
 		function onScroll(event){
 			
 			var scrollPosition = $(document).scrollTop();
